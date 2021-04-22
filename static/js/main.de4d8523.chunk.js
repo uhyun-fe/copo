27d18046e42becbcd13df631961d53c03bc221fa
uhyun-fe <feyouhyun0957@gmail.com>
2021-04-22 12:46:45 +0900

    Updates

--- static/js/main.de4d8523.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcopo=this.webpackJsonpcopo||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(0),o=c.n(n),r=c(3),s=c.n(r),i=c(1),p=function(){return Object(i.jsx)("div",{children:"copo web"})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.de4d8523.chunk.js.map
